d5936f904b6c3b3e4900ff39ac6b4009bf90ed48
keptenkurk <paulmerkx@gmail.com>
2020-05-31 22:58:28 +0200

    PEP8 fixes

--- mxbackup.py
+++ mxbackup.py
@@ -2,13 +2,14 @@
 # * mxbackup.py
 # * Mobotix camera configuration backup utility
 #
-# This script saves configuration files from (multiple) mobotix camera's through
-# the Mobotix API
+# This script saves configuration files from (multiple) mobotix camera's
+# through the Mobotix API
 # See http://developer.mobotix.com/paks/help_cgi-remoteconfig.html for details
 # usage:
 # python mxbackup.py [options]
 # use option -h or --help for instructions
-# See https://github.com/keptenkurk/mxpgm/blob/master/README.md for instructions
+# See https://github.com/keptenkurk/mxpgm/blob/master/README.md for
+# instructions
 #
 # release info
 # 1.0 first release 29/8/17 Paul Merkx
@@ -29,7 +30,8 @@ RELEASE = '1.3beta - 30-5-2020'
 TMPCONFIG = 'config.tmp'
 TIMEOUT = 10  # requests timeout (overwriteable by -t option)
 # Ignore the warning that SSL CA will not be checked
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.
+                                           exceptions.InsecureRequestWarning)
 
 
 def filewritable(filename):
@@ -37,7 +39,8 @@ def filewritable(filename):
         f = open(filename, 'w')
         f.close()
     except IOError:
-        print('Unable to write to ' + filename + '. It might be open in another application.')
+        print('Unable to write to ' + filename +
+              '. It might be open in another application.')
         return False
     os.remove(filename)
     return True
@@ -88,7 +91,8 @@ def transfer(ipaddr, use_ssl, username, password, commandfile):
             else:
                 return True, content
         else:
-            print('HTTP response code: ', HTTPStatus(response.status_code).phrase)
+            print('HTTP response code: ',
+                  HTTPStatus(response.status_code).phrase)
             return False, ''
 
 
@@ -96,23 +100,30 @@ def transfer(ipaddr, use_ssl, username, password, commandfile):
 # *** Main program ***
 # ***************************************************************
 print('MxBackup ' + RELEASE + ' by (c) Simac Healthcare.')
-print('Saves entire configuration of multiple Mobotix camera\'s to local disk.')
+print('Saves entire configuration of multiple Mobotix camera\'s to \
+      local disk.')
 print('Disclaimer: ')
 print('USE THIS SOFTWARE AT YOUR OWN RISK')
 print(' ')
 
 # *** Read arguments passed on commandline
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--deviceIP", nargs=1, help="specify target device IP when reading a single camera")
-parser.add_argument("-l", "--devicelist", nargs=1, help="specify target device list in CSV when reading multiple camera's")
-parser.add_argument("-u", "--username", nargs=1, help="specify target device admin username")
-parser.add_argument("-p", "--password", nargs=1, help="specify target device admin password")
-parser.add_argument("-s", "--ssl", help="use SSL to communicate (HTTPS)", action="store_true")
+parser.add_argument("-d", "--deviceIP", nargs=1, help="\
+                    specify target device IP when reading a single camera")
+parser.add_argument("-l", "--devicelist", nargs=1, help="\
+            specify target device list in CSV when reading multiple camera's")
+parser.add_argument("-u", "--username", nargs=1, help="\
+                    specify target device admin username")
+parser.add_argument("-p", "--password", nargs=1, help="\
+                    specify target device admin password")
+parser.add_argument("-s", "--ssl", help="\
+                    use SSL to communicate (HTTPS)", action="store_true")
 
 args = parser.parse_args()
 
 # *** Check validity of the arguments
-if (args.deviceIP is None and args.devicelist is None) or (args.deviceIP and args.devicelist):
+if (args.deviceIP is None and args.devicelist is None) or \
+   (args.deviceIP and args.devicelist):
     print("Either deviceIP or devicelist is required")
     sys.exit()
 
@@ -128,18 +139,17 @@ if args.password is None:
 else:
     password = args.password[0]
 
-if not args.deviceIP and not args.devicelist:
-    print("No devices specified. Either specify a device (-d) or devicelist (-l)")
-    sys.exit()
-
 if args.deviceIP:
     if not validate_ip(args.deviceIP[0]):
-        print("Warning: The device %s is not a valid IPv4 address!" % (args.deviceIP[0]))
-        print("Continuing using %s as devicename." % (args.deviceIP[0]))
+        print("Warning: The device %s is not a valid IPv4 address!"
+              % (args.deviceIP[0]))
+        print("Continuing using %s as devicename."
+              % (args.deviceIP[0]))
 
 if args.devicelist:
     if not os.path.exists(args.devicelist[0]):
-        print("The devicelist '%s' does not exist in the current directory!" % (args.devicelist[0]))
+        print("The devicelist '%s' does not exist in the current directory!"
+              % (args.devicelist[0]))
         sys.exit()
 
 if args.ssl:
@@ -178,17 +188,18 @@ for devicenr in range(1, len(devicelist)):
             outfile.close()
             ipaddr = devicelist[devicenr][0]
             cfgfilename = ipaddr.replace(".", "-") + "_" + \
-                                         datetime.datetime.now().strftime("%y%m%d-%H%M") + \
-                                         ".cfg"
+                datetime.datetime.now().strftime("%y%m%d-%H%M") + \
+                ".cfg"
             if filewritable(cfgfilename):
-                (result, received) = transfer(ipaddr, use_ssl, username, password, TMPCONFIG)
+                (result, received) = transfer(ipaddr, use_ssl,
+                                              username, password, TMPCONFIG)
                 if result:
                     print('Backup of ' + ipaddr + ' succeeded.')
                     outfile = open(cfgfilename, 'w')
                     outfile.write(received)
                     outfile.close()
 
-                    #remove first 4 and last 3 lines
+                    # remove first 4 and last 3 lines
                     outfile = open(cfgfilename, 'r')
                     configfiledata = outfile.readlines()
                     outfile.close()
@@ -196,9 +207,8 @@ for devicenr in range(1, len(devicelist)):
                     outfile = open(cfgfilename, 'w')
                     for line in range(4, len(configfiledata)-3):
                         outfile.write(configfiledata[line])
-                    outfile.close()                    
+                    outfile.close()
                 else:
                     print('ERROR: Reading of ' + ipaddr + ' failed.')
             os.remove(TMPCONFIG)
-        print('')
 print("Done.")
